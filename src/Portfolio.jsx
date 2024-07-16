99e6fca846744d10ea9ede6f2dd2e3b9d3d6e9a5
Hudson Kass <kass.h@northeastern.edu>
2024-07-16 13:18:44 -0700

    fixed screen sizing issues

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -22,19 +22,19 @@ function Portfolio() {
       </div>
       <div class="Cards">
         <Card name="Pawpilot"
-              objective="Enhance accessibility to computer science education for underprivileged students through an intuitive web platform integrated with AI. Our website breaks down assignments into manageable steps for beginners, fostering independent learning without directly providing solutions."
+              objective="Enhance accessibility to computer science education for underprivileged students through an intuitive web platform integrated with AI. Our website breaks down assignments into manageable steps for beginners, fostering independent learning without directly providing solutions. (Mar 2024-Mar 2024)"
               skills="HTML, CSS, Node.js, API Integration, Resource Management"
               takeaway="Participated in my first hackathon, leading our team to a 1st place victory in the AI category. Learned to effectively use APIs (OpenAI custom GPT models) and the importance of environment variables."/>
         <Card name="Portfolio Website" 
-              objective="Designed to showcase my personal journey and passion for learning, this website serves as a professional portfolio for potential opportunities."
+              objective="Designed to showcase my personal journey and passion for learning, this website serves as a professional portfolio for potential opportunities. (Jun 2024 - Present)"
               skills="HTML, CSS, React, JavaScript"
               takeaway="Built my first website independently using React, learning its structural benefits and lots of CSS troubleshooting in the process."/>
         <Card name="Minecraft Mods" 
-              objective="Created unique Minecraft mods to enhance gameplay, driven by a passion for the game and learning Java at the time."
+              objective="Created unique Minecraft mods to enhance gameplay, driven by a passion for the game and learning Java at the time. (Dec 2023 - Mar 2024)"
               skills="Java, JSON/XML, Problem Solving"
               takeaway="Began learning how to interpret existing code and applied it to create features like a 'metal detector' and a 'digger' item, strengthened my understanding of Java and mod development."/>
         <Card name="Highliner AI" 
-              objective="Contributed to the development of mock-data generation and a simple email-request website for an AI startup."
+              objective="Contributed to the development of mock-data generation and a simple email-request website for an AI startup. (Feb 2024 - Jul 2024)"
               skills="HTML, CSS, Node.js, Python, Artificial Intelligence"
               takeaway="My first corporate experience involved more research than coding, deepening my understanding of AI models and data quality, and emphasizing the importance of version control."/>
         </div>
